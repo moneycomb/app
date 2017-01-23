0a9a13dd8b7e88a345b579c166671d63b5bceb5e
Glenn Schleicher <gschleic@gmail.com>
2017-01-23 09:55:54 -0500

    minor changes

--- config/development/env.sh
+++ config/development/env.sh
@@ -11,15 +11,14 @@ export PLAID_SERVER="https://tartan.plaid.com/"
 #export MONGO_URL="mongodb://glenn:L1beration64@candidate.37.mongolayer.com:11079,candidate.21.mongolayer.com:11057/production?replicaSet=set-5641699de9bf3797d9000015"
 #export MONGO_OPLOG_URL="mongodb://oploguser:L1beration64@candidate.37.mongolayer.com:11079/local?authSource=production"
 # STAGE
-export PLAID_ENV="tartan"
-export PLAID_SERVER="https://tartan.plaid.com/"
-export PLAID_API_ENDPOINT="https://tartan.plaid.com/"
-export MONGO_URL="mongodb://glenn:L1beration64@candidate.21.mongolayer.com:11057,candidate.37.mongolayer.com:11079/stage-db?replicaSet=set-5641699de9bf3797d9000015"
-export MONGO_OPLOG_URL="mongodb://glenn:L1beration64@candidate.21.mongolayer.com:11057,candidate.37.mongolayer.com:11079/local?authSource=stage-db"
-# LOCAL
 #export PLAID_ENV="tartan"
 #export PLAID_SERVER="https://tartan.plaid.com/"
 #export PLAID_API_ENDPOINT="https://tartan.plaid.com/"
-#unset MONGO_URL
-#unset MONGO_OPLOG_URL
-
+#export MONGO_URL="mongodb://glenn:L1beration64@candidate.21.mongolayer.com:11057,candidate.37.mongolayer.com:11079/stage-db?replicaSet=set-5641699de9bf3797d9000015"
+#export MONGO_OPLOG_URL="mongodb://glenn:L1beration64@candidate.21.mongolayer.com:11057,candidate.37.mongolayer.com:11079/local?authSource=stage-db"
+# LOCAL
+export PLAID_ENV="tartan"
+export PLAID_SERVER="https://tartan.plaid.com/"
+export PLAID_API_ENDPOINT="https://tartan.plaid.com/"
+unset MONGO_URL
+unset MONGO_OPLOG_URL
